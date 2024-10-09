493aa92ee622e3d6188ee4fc5d8a2ac9ad7a0cb7
GhostCrusher1208 <157836497+GhostCrusher1208@users.noreply.github.com>
2024-10-09 18:26:55 +0300

    Cillop gibi kuşlar

--- Assets/Scripts/TimeSlow.cs
+++ Assets/Scripts/TimeSlow.cs
@@ -1,25 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class TimeSlow : MonoBehaviour
 {
     [Header("TimeControllerSetting")]
-    public float TargetTimescale=1f;
+    public float TargetTimescale = 1f;
     public float lerpSpeed = 1f;
     private bool isTimeSlowed = false;
 
-   
     public AudioSource source;
     public AudioClip clip;
     public AudioClip Fastclip;
 
-        
+    public Volume volume;
+    private WhiteBalance white; // Class level variable
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        // Get ChannelMixer from Volume Profile and assign it to the class-level variable
+        if (volume.profile.TryGet<WhiteBalance>(out white))
+        {
+            // Modify the initial channel mixer settings
+            white.temperature.value = 0f; // Set initial value
+        }
+        else
+        {
+            Debug.LogWarning("ChannelMixer not found in the Volume Profile.");
+        }
     }
 
     // Update is called once per frame
@@ -31,21 +43,28 @@ public class TimeSlow : MonoBehaviour
             // Zaman yavaþsa normale döndür, deðilse yavaþlat
             if (isTimeSlowed)
             {
-                if(!source.isPlaying) { source.PlayOneShot(Fastclip); }
+                if (!source.isPlaying) { source.PlayOneShot(Fastclip); }
                 TargetTimescale = 1f; // Normal hýza dön
+                if (white != null) // Check if channelMixer is assigned
+                {
+                    white.temperature.value = 0f; // Reset channel value
+                }
                 isTimeSlowed = false;  // Yavaþlatma durumunu güncelle
             }
-            else if(!isTimeSlowed) 
+            else
             {
-                if (!source.isPlaying) { 
-                source.PlayOneShot(clip);
-                                      }// Ses çal
-                TargetTimescale = 0.2f;    // Zamaný yavaþlat
+                if (!source.isPlaying)
+                {
+                    source.PlayOneShot(clip); // Ses çal
+                }
+                TargetTimescale = 0.2f;
+                if (white != null) // Check if channelMixer is assigned
+                {
+                    white.temperature.value = -60f; // Set channel value to 50
+                }
+                // Zamaný yavaþlat
                 isTimeSlowed = true;       // Yavaþlatma durumunu güncelle
             }
-
         }
-       
     }
 }
-
